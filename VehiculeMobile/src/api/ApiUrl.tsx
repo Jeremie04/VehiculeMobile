25f622b71acd7fe1deab8de1b7f8041fd6090789
Jeremie04 <118689934+Jeremie04@users.noreply.github.com>
2024-02-10 11:52:26 +0100

    url

--- VehiculeMobile/src/api/ApiUrl.tsx
+++ VehiculeMobile/src/api/ApiUrl.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 // const ApiUrl = "https://vehiculespring-production-5f1a.up.railway.app/";
 // const ApiUrl = "https://vehiculespring-production-5f1a.up.railway.app/";
-const ApiUrl = "http://localhost:8081";
+const ApiUrl = "https://vehicule-production-87c4.up.railway.app/";
+// const ApiUrl = "http://localhost:8081";
 
 export default ApiUrl;
